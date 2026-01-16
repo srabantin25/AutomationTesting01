32a7fbe47ce661db9c18244e56f205be43e40bb0
srabantin25 <srabanti.n25@gmail.com>
2026-01-16 14:27:30 +0530

    16Dec Task

--- src/task/Dec16_Task.java
+++ src/task/Dec16_Task.java
@@ -5,8 +5,9 @@ import java.util.Scanner;
 public class Dec16_Task {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a new String: ");
         String l = scanner.next();
-        String r = " ";
+        String r = "";
          for (int i= l.length()-1; i>=0; i--)
         {
             r= r + l.charAt(i);
