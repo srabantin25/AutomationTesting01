cf1ab5baaad470c47f64a4c6f34a5eda032dd1d0
srabantin25 <srabanti.n25@gmail.com>
2026-01-16 14:22:15 +0530

    16Dec Task

---
+++ src/task/Dec16_Task.java
@@ -0,0 +1,18 @@
+package task;
+
+import java.util.Scanner;
+
+public class Dec16_Task {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String l = scanner.next();
+        String r = " ";
+         for (int i= l.length()-1; i>=0; i--)
+        {
+            r= r + l.charAt(i);
+
+        }
+        System.out.println(r);
+    }
+
+}
